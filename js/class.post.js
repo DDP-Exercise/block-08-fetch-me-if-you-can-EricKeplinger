40571026521c7f57ab1f390432b3f53be87f1f30
Ricki2001 <s2510892043@students.fh-hagenberg.at>
2026-06-24 18:01:35 +0200

    created the post class

--- js/class.post.js
+++ js/class.post.js
@@ -17,3 +17,21 @@
  *  When printing a post, don't forget to make a button that
  *  loads the comments for the post. Once they are loaded, print them.
  *  *******************************************************/
+
+export default class Post {
+    constructor(data) {
+        this.id = data.id;
+        this.userId = data.userId;
+        this.title = data.title;
+        this.body = data.body;
+        this.comments = null;
+    }
+
+    setComments(comments) {
+        this.comments = comments;
+    }
+
+    commentsAreLoaded() {
+        return this.comments !== null;
+    }
+}
